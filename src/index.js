f29b7ecfd05b688b04b322ae34179b8f478e543c
keyla <keyyleiva@gmail.com>
2023-08-28 15:01:06 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.getElementById('create-task-form')
+  let toDoList = document.getElementById("tasks")
+  let newToDo = document.getElementById('new-task-description')
+ 
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let addTasks = document.createElement('li')
+    toDoList.appendChild(addTasks)
+    addTasks.textContent = newToDo.value
+    e.target.reset()
+    btn = document.createElement('button')
+    btn.textContent = " Finished Task"
+    addTasks.appendChild(btn)
+    btn.addEventListener('click', (e) => {
+      e.target.parentNode.remove()
+     
+    })
+  })  
+})
